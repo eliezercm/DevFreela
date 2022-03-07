ae00f46dc01055cd2175f4e90c5ab4a627d89914
Eliezer Martins <eliezermarttins@gmail.com>
2022-03-07 15:00:08 -0300

    install mediatr and create example queries and handlers

---
+++ DevFreela.Application/Commands/CreateProject/CreateProjectCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Application.Commands.CreateProject
+{
+    public class CreateProjectCommand : IRequest<int>
+    {
+    }
+}
---
+++ DevFreela.Application/Commands/CreateProject/CreateProjectCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Commands.CreateProject
+{
+    public class CreateProjectCommandHandler : IRequestHandler<CreateProjectCommand, int>
+    {
+        public Task<int> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
